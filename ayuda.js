b51ee35de8416a92d770181764ca3aa1a7bfcce1
vemma5729-lgtm <vemma5729@gmail.com>
2026-09-11 20:02:38 -0300

    septiembre

--- ayuda.js
+++
@@ -1,9 +0,0 @@
-const bloques = document.querySelectorAll('.accordeon .bloque');
-
-bloques.forEach(function (bloque) {
-  const pregunta = bloque.querySelector('.h2');
-
-  pregunta.addEventListener('click', function () {
-    bloque.classList.toggle('activo');
-  });
-});
