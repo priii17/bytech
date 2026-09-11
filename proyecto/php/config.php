c0d25ccb548ef8749f8eee5a09ddbdfe14024d95
prii17 <priscialgcas08@gmail.com>
2026-09-11 00:13:18 -0300

    config.php actualizado con la bd en xammpp

--- proyecto/php/config.php
+++ proyecto/php/config.php
@@ -1,8 +1,8 @@
 <?php
 
 define('BD_host', 'localhost');
-define('BD_usuario', 'bytech');
-define('BD_contrasenia', 'Bytech,26');
-define('BD_nombre','db_bytech');
+define('BD_usuario', 'root');
+define('BD_contrasenia', '');
+define('BD_nombre','sistema');
 
 ?> 
